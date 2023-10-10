0a34673da512d72d2f7765219b99bd7d5000e389
vishaldhatrika <dvishal35@gmail.com>
2023-10-10 12:04:20 -0500

    added class

--- src/sprint1/production/Game.java
+++ src/sprint1/production/Game.java
@@ -1,16 +1,80 @@
 package sprint1.production;
 
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 public class Game {
-public static void main(String [] args){
 
-    System.out.println("Game started!");
-    System.out.println(System.getProperty("user.home"));
+ public boolean dirAndFileSetup(){
+
+     String homeDir= System.getProperty("user.home");
+     Path gameDirPath= Paths.get(homeDir,"Mills");
+     boolean gameDirExists=false;
+
+     if(Files.exists(gameDirPath) == false){
+         File gameDir = new File(String.valueOf(gameDirPath));
+         try{
+             gameDirExists=gameDir.mkdir();
+         }
+         catch (Throwable e){
+             System.out.println("Exception in creating Game Directory: ");
+
+             e.printStackTrace();
+             return false;
+         }
+
+     }
+     else{
+         gameDirExists=true;
+     }
+
+     boolean gameFileExists = false;
+
+     if(gameDirExists){
+         Path gameLogFilePath = Paths.get(String.valueOf(gameDirPath),  "gameLogFile.txt");
+         gameFileExists = Files.exists(gameLogFilePath);
+
+         File gameLogFile= new File(String.valueOf(gameLogFilePath));
+
+         if(gameFileExists == false){
+//            gameLogFile = new File(String.valueOf(gameLogFilePath));
+             try {
+                 boolean createdFile =  gameLogFile.createNewFile();
+             } catch (IOException e) {
+                 System.out.println("Exception in creating Game File: ");
+                 e.printStackTrace();
+                 return false;
 
 
 
+             }
+             boolean writePermission = gameLogFile.setWritable(true);
+             boolean readPermission = gameLogFile.setReadable(true);
 
 
+             gameFileExists=true;
+         }
 
-    
-}
+         if(gameLogFile.canRead() && gameLogFile.canWrite()){
+             try{
+                 System.out.println("entered it");
+                 FileWriter writerObj = new FileWriter(gameLogFile,true);
+                 writerObj.write("123\n");
+                 writerObj.close();
+             }
+             catch(IOException e){
+                 System.out.println("File write error.");
+                 e.printStackTrace();
+                 return false;
+             }
+
+         }
+     }
+    return true;
+ }
+
 }
---
+++ src/sprint1/production/GameGUI.java
@@ -0,0 +1,11 @@
+package sprint1.production;
+
+public class GameGUI {
+
+    public static void main(String[] args) {
+        Game game=new Game();
+
+        game.dirAndFileSetup();
+    }
+
+}
